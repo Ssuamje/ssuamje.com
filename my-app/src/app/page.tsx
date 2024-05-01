3c30be69c3e718608755f9b9f2a6f6558f4c66f3
Ssuamje <side2850@naver.com>
2024-05-01 19:07:54 +0900

    FEAT : Dark/Light Mode

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@/prisma/generated/client";
 import Tests from "@/components/tests.server";
 
-const prisma = new PrismaClient();
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
--- my-app/src/components/tests.server.tsx
+++ my-app/src/components/tests.server.tsx
@@ -3,16 +3,18 @@ import { PrismaClient } from "@/prisma/generated/client";
 const prisma = new PrismaClient();
 
 export default async function Tests() {
-    const tests =  await prisma.test.findMany();
+  const tests = await prisma.test.findMany();
 
-    return (
-      <div>
-        <h1>User List</h1>
-  <ul>
-  {tests.map(user => (
-      <li key={user.id}>{user.name} - {user.email}</li>
-    ))}
-  </ul>
-  </div>
-    )
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {tests.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
